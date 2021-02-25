b01ec4488ce8ea2abab00e3ddfe17eaf18bc7f66
Rodrigo N <rodrigo.gomes65@etec.sp.gov.br>
2021-02-25 13:04:19 -0300

    QoL: basic code cleaning

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # LEIA COM ATENÇÃO
-# 
+# APIs usadas: ChartJS, Axios.
 #
 # Devido a diferença de preço entre as moedas, é possível que o gráfico fique visualmente feio caso uma criptomoeda de alto valor em relação as outras seja inserida.
 # Por isso, optei por remover a criptomoeda Bitcoin como moeda padrão ao iniciar o web app - a mesma agora está presente na lista de moedas no select.
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//global variables
+//#GV - global variables
 const updateTimeMs = 30000;
-let cryptos = ['eth'];
+let currentCryptos = ['eth'];
 class Crypto {
     constructor(label, color, dataIndex, data){
         this.label = label;
@@ -8,21 +8,19 @@ class Crypto {
         this.borderColor = color;
         this.data[dataIndex] = data;
     }
-    // label = 'TESTE-R$';
-    // backgroundColor = 'rgb(55, 99, 232)';
-    // borderColor = 'rgb(55, 99, 232)';
+
     data = [];
     fill = {
         Disable: true
     }
 }
 
-//querySelectors
+//#QS - querySelectors
 const addCurrencyButton = document.querySelector('#confirm');
 const timeCounter = document.querySelector('#seconds');
 timeCounter.innerText = updateTimeMs/1000;
 
-//functions
+//#FC - functions
 async function getValue(crypto){
     let req = await axios.get(`https://api.cryptonator.com/api/ticker/${crypto}-brl`);
     let value = req.data.ticker.price;
@@ -30,14 +28,13 @@ async function getValue(crypto){
     return value;
 }
 
-
 function addData(chart, label, data, dataset) {
     chart.data.labels.push(label);
     chart.data.datasets[dataset].data.push(data);
     chart.update();
 }
 //needed to avoid duplicated labels
-function addData2(chart, data, dataset){
+function addDataToExistingSets(chart, data, dataset){
     chart.data.datasets[dataset].data.push(data);
     chart.update();
 }
@@ -50,28 +47,32 @@ function randomRGB(){
     console.log(rgbString);
     return rgbString;
 }
+function createNewCrypto(){
+    const opt = document.querySelector('#cryptoSelector');
+    let label = opt.value;
+    let value = await getValue(newLabel);
+    currentCryptos.push(label)
+    label = label.toUpperCase() + '-R$';
+    let chartLength = chart.data.labels.length;
+    
+    console.log(label, value);
+    let newCrypto = new Crypto(label,randomRGB(),chartLength-1,value);
 
-//initial data
+    return newCrypto;
+}
+//Chart's initial data
 window.addEventListener('load',async()=>{
-    let initialValue = await getValue(cryptos[0]);
+    let initialValue = await getValue(currentCryptos[0]);
     addData(chart,'',initialValue,0);
 })
 
 
 
-//updates
+//#UPD - Chart updates
 addCurrencyButton.addEventListener('click', async (event)=>{
-    const opt = document.querySelector('#cryptoSelector');
-    event.preventDefault();
-    let newLabel = opt.value;
-    let newValue = await getValue(newLabel);
-    cryptos.push(newLabel)
-    newLabel = newLabel.toUpperCase() + '-R$';
-    let currLength = chart.data.labels.length;
     
-    console.log(newLabel, newValue);
-    let newCrypto = new Crypto(newLabel,randomRGB(),currLength-1,newValue);
-    ;
+    event.preventDefault();
+    let newCrypto = createNewCrypto();
     chart.data.datasets.push(newCrypto);    
     chart.update();
     opt.options[opt.options.selectedIndex].setAttribute('disabled','disabled');
@@ -82,11 +83,11 @@ window.setInterval(async()=>{
     let timeStamp = new Date();
     timeStamp = timeStamp.toLocaleTimeString('pt-BR');
     let i = 0;   
-    for(const crypto of cryptos){  
+    for(const crypto of currentCryptos){  
         let newValue = await getValue(crypto);
         console.log(timeStamp, newValue);
         if(i > 0){
-            addData2(chart,newValue,i);
+            addDataToExistingSets(chart,newValue,i);
         }else{
             addData(chart,timeStamp,newValue,i);
         } 
@@ -94,6 +95,7 @@ window.setInterval(async()=>{
     }
     
 },updateTimeMs);
+
 window.setInterval(async ()=>{
     let remainingTime = timeCounter.innerText;
     timeCounter.innerText = remainingTime-1;
@@ -103,8 +105,8 @@ window.setInterval(async ()=>{
 },1000);
 
 //chart
-const ctx = document.getElementById('myChart').getContext('2d');
-let chart = new Chart(ctx, {
+const mainChart = document.getElementById('myChart').getContext('2d');
+let chart = new Chart(mainChart, {
     // The type of chart we want to create
     type: 'line',
 
@@ -136,19 +138,6 @@ let chart = new Chart(ctx, {
         }
     }
 });
-function chartInit(chart, floor, steps){
-    chart.options = {
-        scales:{
-            yAxes: [{
-                ticks:{
-                    min: floor,                  
-                    stepSize: steps
-                }
-            }]
-        }
-    };
-    chart.update();
-};
 
 
 
